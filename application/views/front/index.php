0d49ed6ca9cc1c14a7e76294e65d47fb63b7655a
rizqinusabbih <rizqi.ffa@gmail.com>
2024-08-24 16:33:22 +0700

    First commit

---
+++ application/views/front/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Siimple - Bootstrap Landing Page Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="<?php echo base_url('assets/template/front/'); ?>img/favicon.png" rel="icon">
+    <link href="<?php echo base_url('assets/template/front/'); ?>img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?php echo base_url('assets/template/front/'); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/template/front/'); ?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?php echo base_url('assets/template/front/'); ?>css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Siimple
+  * Template URL: https://bootstrapmade.com/free-bootstrap-landing-page/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    <main id="main">
+
+        <!-- ======= Why Us Section ======= -->
+        <section id="why-us" class="why-us section-bg">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="card">
+                            <img src="assets/img/why-us-1.jpg" class="card-img-top" alt="...">
+                            <div class="card-icon">
+                                <i class="bx bx-book-reader"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">Our Mission</a></h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="card">
+                            <img src="assets/img/why-us-2.jpg" class="card-img-top" alt="...">
+                            <div class="card-icon">
+                                <i class="bx bx-calendar-edit"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">Our Plan</a></h5>
+                                <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="card">
+                            <img src="assets/img/why-us-3.jpg" class="card-img-top" alt="...">
+                            <div class="card-icon">
+                                <i class="bx bx-landscape"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"><a href="">Our Vision</a></h5>
+                                <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End Why Us Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Cybermansa</span></strong>. All Rights Reserved
+            </div>
+        </div>
+    </footer><!-- End #footer -->
+
+    <!-- Vendor JS Files -->
+    <script src="<?php echo base_url('assets/template/front/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url('assets/template/front/'); ?>vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?php echo base_url('assets/template/front/'); ?>js/main.js"></script>
+
+</body>
+
+</html>
